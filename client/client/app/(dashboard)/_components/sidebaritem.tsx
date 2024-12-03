282d05c34624c4a636976a465c7a2c0168136fc8
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-12-03 01:05:14 +0100

    tailwinding until i dont tailwind no more

--- client/client/app/(dashboard)/_components/sidebaritem.tsx
+++ client/client/app/(dashboard)/_components/sidebaritem.tsx
@@ -1,5 +1,7 @@
 import { LucideIcon } from "lucide-react"
 import { usePathname, useRouter} from "next/navigation"
+import {cn} from '../../../libs/utils'
+
 
 interface SideBarItemProps {
     icon : LucideIcon ,
@@ -13,9 +15,25 @@ export const SideBarItem = ({icon: Icon, label, href} : SideBarItemProps) => {
 
     const pathname = usePathname();
     const router = useRouter();
+    const isActive = 
+        (pathname == "/" && href == "/") || 
+        pathname == href || 
+        pathname?.startsWith(`${href}/`);
+
+    const onClick = () => {
+        router.push(href);
+    }
 
 
     return (
-        <div>side bar item </div>
+        <button
+            onClick={onClick}
+            type="button"
+            className={cn(
+                    "flex items-center gap-x-2 text-slate-500 test-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20", isActive && "text-green-700 bg-sky-200/20 hover:bg-sky-200/20 "
+            )}
+        >
+
+        </button>
     )
 }
